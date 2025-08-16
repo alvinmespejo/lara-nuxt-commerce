5a2846d0916c04d89fc91a45363b6fa2a57f213a
Alvin Espejo <espejo.a2@gmail.com>
2025-08-16 16:41:41 +0800

    Removed unused controllers and methods

--- server/app/Http/Controllers/Api/v1/OrderController.php
+++ server/app/Http/Controllers/Api/v1/OrderController.php
@@ -85,36 +85,4 @@ class OrderController extends Controller implements HasMiddleware
             return new ApiResponseError($th);
         }
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Order $order)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Order $order)
-    {
-        //
-    }
 }
